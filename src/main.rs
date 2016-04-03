578b5b17eb7aef6e0ce867116d2625b8254199a5
Nipunn Koorapati <nipunn@dropbox.com>
2016-04-02 17:13:07 -0700

    Add a quicksort

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,52 @@
+use std::cmp::Ordering;
 use std::io;
+use std::io::BufRead;
 
 fn main() {
-    let lines = io::stdin().lines();
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let lines = stdin.lock().lines();
+    let mut vect: Vec<i32> = Vec::new();
+
+    for line in lines {
+        let line = line.expect("Failed to read line");
+        let val = line.parse().expect("Must enter an integer");
+        vect.push(val);
+    }
+
+    for val in &vect {
+        println!("before: {}", val);
+    }
+
+    qsort(&mut vect[..]);
+
+    for val in &vect {
+        println!("after: {}", val);
+    }
+}
+
+fn qsort(slice: &mut [i32]) {
+    if slice.len() <= 1 {
+        return
+    }
+    let partition_idx = partition(slice);
+    qsort(&mut slice[..partition_idx]);
+    qsort(&mut slice[partition_idx + 1..]);
+}
+
+fn partition(slice: &mut [i32]) -> usize {
+    /* Partition the slice around an element and return the idx of that
+       element in linear time.
+    */
+    let mut partition_idx = 0;
+    for i in 1..slice.len() {
+        match slice[i].cmp(&slice[partition_idx]) {
+            Ordering::Less => {
+                slice.swap(i, partition_idx + 1);
+                slice.swap(partition_idx, partition_idx + 1);
+                partition_idx += 1;
+            }
+            _ => ()
+        }
+    }
+    return partition_idx;
 }
