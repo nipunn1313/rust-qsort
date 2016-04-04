d50a0569976c367136a801b29ebd902c735bcfd7
Nipunn Koorapati <nipunn@dropbox.com>
2016-04-03 19:06:01 -0700

    Use getopt to use command line args to prepare for another main impl

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
+extern crate getopts;
+
 mod qsort;
 
+use getopts::Options;
+use std::env;
 use std::io;
 use std::io::BufRead;
+
 use qsort::qsort;
 
+fn halp(opts: Options, prog_name: &String) {
+    println!("{}", opts.usage(&opts.short_usage(prog_name)));
+}
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let prog_name = args[0].clone();  // Grab prog name before args are consumed
+
+    let mut opts = Options::new();
+    opts.optflag("", "stdin", "Read values from stdin to sort");
+    opts.optflag("h", "help", "Print usage");
+    let matches = match opts.parse(args) {
+        Ok(m) => m,
+        Err(_) => {
+            return halp(opts, &prog_name);
+        }
+    };
+    if matches.opt_present("h") {
+        return halp(opts, &prog_name);
+    }
+
+    if matches.opt_present("stdin") {
+        sort_from_stdin();
+    } else {
+        // TODO(implement something more cool?)
+    }
+}
+
+fn sort_from_stdin() {
     let stdin = io::stdin();
     let lines = stdin.lock().lines();
     let mut vect: Vec<i32> = Vec::new();
